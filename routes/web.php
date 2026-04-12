a4a9769f9c70bb4407e5a67f7206ba38a662a86d
UserRic <enrico.dbulatao@gmail.com>
2026-04-12 21:08:53 +0800

    Initial Laravel project upload

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/test-admin', function () {
+    return "ADMIN ACCESS GRANTED";
+})->middleware(['auth', 'role:admin']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])
+    ->get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+require __DIR__.'/auth.php';
